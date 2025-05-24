9737a68b1b6a51b1f459f696916542ce0b72d2da
pimphand <faisaldwiki69@gmail.com>
2025-05-24 13:29:30 +0700

    add payment DC

--- routes/api.php
+++ routes/api.php
@@ -35,7 +35,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
     //admin
     Route::apiResource('orders', App\Http\Controllers\Api\OrderController::class);
-    Route::apiResource('orders/add-payment/{id}', App\Http\Controllers\Api\OrderController::class);
+    Route::post('orders/add-payment/{id}', [App\Http\Controllers\Api\OrderController::class,'addPayment']);
 
     //group admin routes
     Route::group(['prefix' => 'admin'], function () {
