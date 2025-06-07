5274aae5731ebb155b31e592e0502c6bf8f3988e
pimphand <faisaldwiki69@gmail.com>
2025-06-07 18:41:23 +0700

    add customer

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\CustomerResource;
 use App\Models\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Validator;
 
 class CustomerController extends Controller
 {
@@ -36,7 +38,27 @@ class CustomerController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = Validator::make($request->all(), [
+            'name' => 'required',
+            'phone' => 'required|numeric|digits_between:10,13|unique:customers,phone',
+            'address' => 'required|',
+            //            'owner_address' => 'required',
+            'store_name' => 'required',
+            'store_photo' => 'required',
+            'owner_photo' => 'nullable',
+            'identity' => 'nullable',
+            'npwp' => 'nullable',
+            'others' => 'nullable',
+            'city' => 'required',
+            'state' => 'required',
+        ]);
+
+        Auth::user()->customers()->create(array_merge($validated->validated(), [
+            'store_photo' => asset('storage') . '/' . $request->file('store_photo')->store('customer/store_photo', 'public'),
+            'owner_photo' => asset('storage') . '/' . $request->file('owner_photo')->store('customer/owner_photo', 'public'),
+        ]));
+
+        return response()->json(['message' => 'Customer created'], 201);
     }
 
     /**
